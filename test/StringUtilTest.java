fa7b71e0519ab0cdb6d51ddc36d21d7501a5e88a
ffaiip <kawintip41@gmail.com>
2018-09-06 10:25:43 +0700

    add more tests

--- test/StringUtilTest.java
+++ test/StringUtilTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 /**
  * Tests of the StringUtil methods.
+ * @author Kavinthip
  */
 public class StringUtilTest {
 
@@ -16,7 +17,29 @@ public class StringUtilTest {
 	public void testIndexOf() {
 		String[] fruit = {"Apple", "Banana", "Grapes"};
 		assertEquals(0, StringUtil.indexOf("Apple", fruit));
+		assertEquals(-1, StringUtil.indexOf("Orange", fruit));
 	}
 
 	//TODO write more tests. Test other cases.
+	
+	@Test
+	public void testBlankIndexOf() {
+		String[] food = {"Chicken", "", "Steak"};
+		assertEquals(1, StringUtil.indexOf("", food));
+	}
+	
+	@Test
+	public void testNextLineIndexOf() {
+		String[] flower = {"Rose", "Lily", "Lavender\n","Jasmine"};
+		assertEquals(2, StringUtil.indexOf("Lavender\n", flower));
+	}
+	
+	@Test
+	public void testTabIndexOf() {
+		String[] nut = {"Almond", "Pecan", "Pistachio\t"};
+		assertEquals(2, StringUtil.indexOf("Pistachio\t", nut));
+	}
+	
+	
+	
 }
